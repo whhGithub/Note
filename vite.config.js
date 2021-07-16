242d9924f21a9d81f3e6b8a71ae9ceabdce30c4b
whhGithub <2698981813@qq.com>
2021-07-16 22:07:15 +0800

    add pwa application

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,30 @@ import postcssNesting from 'postcss-nesting';
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(),
+    plugins: [
+        vue(),
         VitePWA({
             manifest: {
-                // content of manifest
+                "name": "Note",
+                "short_name": "Note",
+                "start_url": "/",
+                "icons": [{
+                        "src": "./icons/manifest-icon-192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable any"
+                    },
+                    {
+                        "src": "./icons/manifest-icon-512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable any"
+                    }
+                ],
+                "theme_color": "#000000",
+                "background_color": "#ffffff",
+                "display": "standalone",
+                "orientation": "portrait"
             },
             workbox: {
                 // workbox options for generateSW
